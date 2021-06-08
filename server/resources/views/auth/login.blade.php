9a94c94397ce459ad5478da1f766266507434868
NGUYEN112 <phannguyenhoainguyen@gmail.com>
2021-06-08 14:01:15 +0700

    Create AuthController & Auth View

---
+++ server/resources/views/auth/login.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.auth-layout')
+@section('login-content')
+    <div class="limiter">
+        <div class="container-login100 small-bg">
+            <div class="wrap-login100 small-bg inside-form">
+
+                <form class="login100-form validate-form" method="POST">
+                    @csrf
+                    <span class="login100-form-title title-login">
+                        Member Login
+                    </span>
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="email" placeholder="Email" required>
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="password" placeholder="Password" required>
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <input class="login100-form-btn" type="submit" value="Login">
+
+                    </div>
+
+
+                    <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                        <a class="txt2" href="#">
+                            Username / Password?
+                        </a>
+                    </div>
+
+                    <div class="text-center p-t-136">
+                        <a class="txt2" href="{{route('cinema.registerpage')}}">
+                            Create your Account
+                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/login',[AuthController::class,'loginPage'])->name('cinema.loginpage');
+Route::post('/login',[AuthController::class,'login'])->name('cinema.login');
+Route::get('/register',[AuthController::class,'registerPage'])->name('cinema.registerpage');
+Route::post('/register',[AuthController::class,'register'])->name('cinema.register');
+Route::get('/logout',[AuthController::class,'logout'])->name('cinema.logout');
+
 
 Route::get('{any}', function () {
     return view('client');
