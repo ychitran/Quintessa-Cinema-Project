79506a596968f1d85d6eb81f3c22aa5bd33a9c1e
NGUYEN112 <phannguyenhoainguyen@gmail.com>
2021-07-06 09:13:34 +0700

    fix order in ticket count

--- client/src/app/manager/seat/list/list.component.ts
+++ client/src/app/manager/seat/list/list.component.ts
@@ -1,8 +1,11 @@
 import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cinema } from 'src/app/_shared/models/cinema.model';
+import { Combo } from 'src/app/_shared/models/combo.model';
 import { Film } from 'src/app/_shared/models/film.model';
+import { Member } from 'src/app/_shared/models/member.model';
 import { Order } from 'src/app/_shared/models/order.model';
 import { Room } from 'src/app/_shared/models/room.model';
 import { Screening } from 'src/app/_shared/models/screening.model';
@@ -16,93 +19,155 @@ import { SeatService } from '../seat.service';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  films : Array<Film>
+  public_films : Array<Film>
+  unpublic_films: Array<Film>
+  films: Array<Film>
   film : Film
+  combos: Array<Combo>
+  film_id : number
   move_name : string
   orderTicketForm : FormGroup
   orderTicket : TicketDetail 
   seats : Array<Seat> = []
   dates : Array<Screening> = []
   start_times : Array<Screening> = []
+  rooms: Array<Screening> = []
   orders : Array<Order> = []
+
   status: boolean = false
   date : Date
   start_time : Time
   seat = []
   screening_id: number
+  combo_id:number
+  combo_name:string
+  combo_price:number
+  count_ticket : Number
+  room_id:number
+  format_price: number
+  total_price: number = 0;
+  seat_id = []
+  user_id :number
+
+  member : Member
+  check_auth :true
   constructor(
-    private readonly seatService: SeatService,
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute
+    private readonly seatService:SeatService,
+    private formBuilder:FormBuilder,
+    private route: ActivatedRoute,
+    private readonly router: Router
+
   ) { }
 
   ngOnInit(): void {
-
+    // this.getAll();
     this.loadFilm();
     this.ticketForm();
+    // this.loadCombo();
   }
 
-  loadFilm():void {
-    this.seatService.getFilms().subscribe(res=>this.films =res)
+  loadCombo() {
+    this.seatService.getCombos().subscribe(res =>this.getProductList(res));
+  }
+  getProductList(res): void {
+    this.combos = res.data;
   }
   
+
+  searchUser(event): void {
+    this.seatService.getUser(event).subscribe(res => this.getUserInfo(res))
+  }
+  getUserInfo(res): void {
+    this.member = res;
+  }
+
   ticketForm() {
     this.orderTicketForm = this.formBuilder.group({
       film_id:[],
-      phone_number: [],
       user_id:[],
       screening_id:[],
-      combo_id:[],
       discount_id:[],
       status:[],
-      ticket_price:[],
       total_price:[],
-      seats: []
-    })
+      seats: [],
+      seat_id: [],
+    })  
   }
   
+  loadFilm() {
+    this.seatService.getFilms().subscribe(res => this.loadFilmStatus(res))
+  }
+  loadFilmStatus(res): void {
+    this.public_films = res.publics
+    this.unpublic_films = res.unpublics
+  }
+  // getAll() {
+  //   this.seatService.getAll().subscribe(res => { this.films = res;})
+  // }
+
+  getProductDetail(event):void {
+    this.combo_id = event
+    this.seatService.getComboDetail(event).subscribe(res => this.loadProductDetail(res))
+  }
+  loadProductDetail(res): void {
+    this.combo_name = res.product_name;
+    this.combo_price = res.product_value;
+  }
 
   loadScreeningDate(event):void {
+    this.film_id = event
+    this.dates = []
     this.seatService.getDate(event).subscribe(res => this.getDateDetail(res))
+    this.seatService.getFilmPrice(event).subscribe(res => this.getPrice(res))
+  }
+  getPrice(res){
+    this.format_price = res;
   }
 
   loadFilmDetail(id):void{
     this.seatService.getFilm(id).subscribe(res=> this.filmDetail(res));
   }
   filmDetail(res): void {
-    this.film =  res[0];
+    this.film =  res;
     this.move_name = this.film.global_name
-    this.patchFormValue(this.film.id)
   }
 
 
-  patchFormValue(id): void {
+  patchFormValue(id,screening_id,combo_id,combo_price,total_price,seat,seat_id,member): void {
       this.orderTicketForm.patchValue({
       film_id:id,
-      phone_number: null,
-      user_id:null,
-      screening_id:this.screening_id,
-      combo_id:null,
+      user_id:member.id,
+      screening_id:screening_id,
       discount_id:null,
-      status:1, 
-      ticket_price:45000,
-      total_price:60000,
-      seats: this.seat
-    })
+      status:0, 
+      total_price:total_price,
+      seats: seat,
+      seat_id : seat_id
+        })
   }
+
+  
 
   save() {
     if(this.orderTicketForm.invalid){
-      alert('Dữ liệu không hợp lệ !');
+      alert('Xin chọn đầy đủ thông tin vé !');
       return;
     }
 
     const { value } = this.orderTicketForm;
-
+    let string = ''
+    for(let i=0; i< value.seats.length;i++) {
+      string =  value.seats[i]+ ' ' + string ;
+    }
+    value.seats = string
     this.seatService.save(value).subscribe(
-      res =>
-      alert('Chỉnh sửa thành công'),
-      err => alert('Chỉnh sửa thất bại')      
+      res =>{
+        alert('Đặt vé thành công')
+          
+        this.router.navigateByUrl('/list-film')
+      }
+      ,
+      err => alert('Đệch con mợ mày')      
     )
   }
 
@@ -111,48 +176,85 @@ export class ListComponent implements OnInit {
   }
 
   loadStartTime(event):void {
-    this.date = event;
-    this.seatService.getStartTime(event).subscribe(res => this.getStartTimeDetail(res))
+    this.date = event
+    this.start_times = []
+    this.seatService.getStartTime(event,this.film_id).subscribe(res => this.getStartTimeDetail(res))
   }
   getStartTimeDetail(res) {
     this.start_times = res
   }
 
-  loadSeat(room_id, screening_id,start_time):void{
-    this.start_time = start_time,
-    this.screening_id = screening_id,
+  loadRoom(start_time) {
+    this.rooms = []
+    this.start_time = start_time
+    this.seatService.getRoom(this.film_id,this.date,start_time).subscribe(res => this.getRoomList(res))
+  }
+  getRoomList(res): void {
+    this.rooms = res;
+  }
+
+  loadScreeningId(room_id) {
+    this.room_id = room_id
+    this.seatService.getScreeningId(this.film_id,this.date,this.start_time,room_id).subscribe(res => this.getScreeningId(res))
+  }
+  getScreeningId(res): void {
+    this.screening_id = res.id
+    this.loadSeat(this.screening_id,this.room_id)
+  }
+
+
+  loadSeat(screening_id,room_id):void{
     this.seatService.getSeat(room_id,screening_id).subscribe(res=>this.getSeatDetail(res));
   }
     getSeatDetail(res) {
     this.seats=res;
   }
 
-  getSeatValue(event) {
+  // disableSeat(seat_id) {
+    
+  // }
+
+  getSeatValue(event,seat_id) {
     const value = event.target.value;
     this.seat.push(value);
+    this.seat_id.push(seat_id);
+    this.total_price = this.format_price * this.seat.length + this.combo_price
   }
 
-  removeSeatValue(event) {
+  removeSeatValue(event,seat_id) {
      
     const value = event.target.value;
     for(let j = 0; j <= this.seat.length; j++) {
       if(value == this.seat[j]){
         this.seat.splice(j,1);
       }
+    };
+    for(let i = 0; i <= this.seat_id.length; i++) {
+      if(seat_id == this.seat_id[i]){
+        this.seat_id.splice(i,1);
+      }
     }
+
   }
 
   clearSeat(){
+    console.log(this.seat.length)
+    for(let i = 0;i<this.seat.length;i++){
+      const seat_select = document.getElementById('seatSelected')
+      seat_select.classList.remove('seat-select')
+    }
     this.seat = []
+    this.total_price = 0
   }
 
-  seatSelect(event) {
+  seatSelect(event,seat_id) {
     if(event.target.classList.contains('seat-select')) {
       event.target.classList.remove('seat-select');
-      this.removeSeatValue(event);
+      this.removeSeatValue(event,seat_id);
     } else {
       event.target.classList.add('seat-select');
-      this.getSeatValue(event);
+      this.getSeatValue(event,seat_id);
+      this.patchFormValue(this.film.id,this.screening_id,this.combo_id,this.combo_price,this.total_price,this.seat,this.seat_id,this.member)
     }
   }
 }
